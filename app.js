e5030d7f7a6193e2520de3e04a0274f9704124d5
rudyjm3 <rudyjm3@yahoo.com>
2020-07-24 17:14:00 -0400

    setup the js coode in app.js to map the menu and return items dynamical / html.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// items
 const menu = [
   {
     id: 1,
@@ -72,3 +73,26 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function() {
+  let displayMenu = menu.map(function(item){
+    //console.log(item);
+
+    //return item;
+    return `<article class="menu-item">
+      <img src="${item.img}" class="photo" alt="${item.title}">
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">$${item.price}</h4>
+        </header>
+        <p class="item-text">${item.desc}</p>
+      </div>
+    </article>`
+  });
+  displayMenu = displayMenu.join("")
+  //console.log(displayMenu);
+  sectionCenter.innerHTML = displayMenu;
+});
